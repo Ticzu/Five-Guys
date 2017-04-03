3bd2d4172460d486bcbf43a6335c3175afae751b
Ticzu <Jeremy@dhcp-wifi-8021x-155-41-103-255.bu.edu>
2017-04-03 18:01:43 -0400

    Revert "Merge remote-tracking branch 'origin/firebase-branch'"
    This reverts commit 2a59954cea1445689b87e67e436bfdb9036335f5, reversing
    changes made to b3a5b142e0de8d01b6a07694d6d24ab8d38563d9.

--- hw_challenge/src/main/java/com/example/hw_challenge/Register.java
+++ hw_challenge/src/main/java/com/example/hw_challenge/Register.java
@@ -4,18 +4,11 @@ import android.app.Activity;
 import android.content.Context;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -31,13 +24,10 @@ import java.net.URL;
 public class Register extends Activity {
     EditText username, fname, lname, password;
     String Username, Firstname, Lastname, Password;
-    TextView display;
     Button btnSubmit;
-    DatabaseReference myRef;
     Context ctx = this;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registerview);
         username = (EditText) findViewById(R.id.edtUser);
@@ -45,42 +35,22 @@ public class Register extends Activity {
         fname = (EditText) findViewById(R.id.edtfname);
         lname = (EditText) findViewById(R.id.edtlname);
         btnSubmit = (Button) findViewById(R.id.btnSubmit);
-        display = (TextView)findViewById(R.id.display);
 
-        myRef = FirebaseDatabase.getInstance().getReference();
-
-        myRef.child("Users").child("user4").child("password").addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d("sucess", "Value is: " + value);
-                display.setText(value);
-            }
-
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w("failure", "Failed to read value.", error.toException());
-            }
-        });
-
-
-        btnSubmit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnSubmit.setOnClickListener(new View.OnClickListener(){
+            @Override public void onClick(View v) {
                 Username = username.getText().toString();
                 System.out.println(Username);
                 Firstname = fname.getText().toString();
                 Lastname = lname.getText().toString();
                 Password = password.getText().toString();
                 ThreadTask threadTask = new ThreadTask();
-                threadTask.execute(Username, Password, Firstname, Lastname);
-                Toast.makeText(ctx, "Congratulations!", Toast.LENGTH_SHORT).show();
+                threadTask.execute(Username,Password, Firstname, Lastname);
+                Toast.makeText(ctx, "Congratulations!", Toast.LENGTH_SHORT ).show();
             }
         });
     }
+
+
 
 
     public void register(View view) {
@@ -90,53 +60,50 @@ public class Register extends Activity {
         Lastname = lname.getText().toString();
         Password = password.getText().toString();
         ThreadTask threadTask = new ThreadTask();
-        threadTask.execute(Username, Password, Firstname, Lastname);
+        threadTask.execute(Username,Password, Firstname, Lastname);
     }
 
 
-    class ThreadTask extends AsyncTask<String, String, String> {
+    class ThreadTask extends AsyncTask <String, String, String> {
 
-        @Override
-        protected String doInBackground(String... params) {
+        @Override protected String doInBackground(String... params) {
             String username = params[0];
             String password = params[1];
             String firstname = params[2];
             String lastname = params[3];
             String data = "";
             int tempt;
-
-            DatabaseReference User = myRef.child("Users").child(username);
-            User.child("password").setValue(password);
-            User.child("firstname").setValue(firstname);
-            User.child("lastname").setValue(lastname);
-
-//            try{
-//                URL url = new URL("http://155.41.92.23/MYCODE/register.php");
-//                String urlParams = "username="+username+"&password="+password+"&firstname="+firstname+"&lastname="+lastname;
-//                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-//                httpURLConnection.setDoOutput(true);
-//                OutputStream os = httpURLConnection.getOutputStream();
-//                os.write(urlParams.getBytes());
-//                os.flush();
-//                os.close();
-//                InputStream is = httpURLConnection.getInputStream();
-//                while((tempt = is.read()) != -1) {
-//                    data += (char)tempt;
-//                }
-//                is.close();
-//                httpURLConnection.disconnect();
-//                return data;
-//            } catch (MalformedURLException e) {
-//                e.printStackTrace();
-//                return e.getMessage();
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//                return e.getMessage();
-//            }
-            //   }
-            return "haha";
+            try{
+                URL url = new URL("http://155.41.92.23/MYCODE/register.php");
+                String urlParams = "username="+username+"&password="+password+"&firstname="+firstname+"&lastname="+lastname;
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setDoOutput(true);
+                OutputStream os = httpURLConnection.getOutputStream();
+                os.write(urlParams.getBytes());
+                os.flush();
+                os.close();
+                InputStream is = httpURLConnection.getInputStream();
+                while((tempt = is.read()) != -1) {
+                    data += (char)tempt;
+                }
+                is.close();
+                httpURLConnection.disconnect();
+                return data;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                return e.getMessage();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return e.getMessage();
+            }
         }
 
+        @Override protected void onPostExecute(String s) {
+            if (s.equals("")) {
+                s = "Data Successfully Saved";
+            }
+            Toast.makeText(ctx, s, Toast.LENGTH_SHORT ).show();
+        }
     }
 
 }
