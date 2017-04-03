5f00ffdd80b036216ded048c6f1604fb96b4bf62
czl1208 <czl1208@bu.edu>
2017-04-03 16:43:06 -0400

    top5_initial

---
+++ hw_challenge/src/main/java/com/example/hw_challenge/TopFiveActivity.java
@@ -0,0 +1,13 @@
+package com.example.hw_challenge;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TopFiveActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.top_five);
+    }
+}
