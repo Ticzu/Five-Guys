10802746470d45ebf23cda537138622cc3cb190c
qm930708 <qm930708@bu.edu>
2017-04-03 23:21:23 -0400

    done1

--- hw_challenge/src/main/java/com/example/hw_challenge/TopFiveActivity.java
+++ hw_challenge/src/main/java/com/example/hw_challenge/TopFiveActivity.java
@@ -1,7 +1,11 @@
 package com.example.hw_challenge;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
@@ -9,21 +13,97 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class TopFiveActivity extends AppCompatActivity {
 
     DatabaseReference myRef;
+    ArrayList<String> userNames;
+    ArrayList<Integer> scores;
+    String[] uNames;
+    int[] hiscores;
+    int count = 4;
+    private TextView player1;
+    private TextView player2;
+    private TextView player3;
+    private TextView player4;
+    private TextView player5;
+    private TextView cplayer;
+    private TextView tvScore1;
+    private TextView tvScore2;
+    private TextView tvScore3;
+    private TextView tvScore4;
+    private TextView tvScore5;
+    private TextView[] players;
+    private TextView[] tvScores;
+    private Button btBack;
+    private String usr;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.top_five);
+//        count = 5;
+        uNames = new String[5];
+        hiscores = new int[5];
+
+        //usrname
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            usr = extras.getString("curr_username");
+
+        }
+
+        player1 = (TextView) findViewById(R.id.player1);
+        player2 = (TextView) findViewById(R.id.player2);
+        player3 = (TextView) findViewById(R.id.player3);
+        player4 = (TextView) findViewById(R.id.player4);
+        player5 = (TextView) findViewById(R.id.player5);
+
+        tvScore1 = (TextView) findViewById(R.id.tvScore1);
+        tvScore2 = (TextView) findViewById(R.id.tvScore2);
+        tvScore3 = (TextView) findViewById(R.id.tvScore3);
+        tvScore4 = (TextView) findViewById(R.id.tvScore4);
+        tvScore5 = (TextView) findViewById(R.id.tvScore5);
+        btBack = (Button) findViewById(R.id.btBack);
+        btBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), hw_challenge.class);
+                i.putExtra("curr_username", usr);
+                startActivity(i);
+            }
+        });
+
+        players = new TextView[]{player1, player2, player3, player4, player5};
+        tvScores = new TextView[]{tvScore1, tvScore2, tvScore3, tvScore4, tvScore5};
+//        userNames = new ArrayList<>();
+//        scores = new ArrayList<>();
 
         myRef = FirebaseDatabase.getInstance().getReference();
 
         myRef.child("Users").orderByChild("hiscore").limitToLast(5).addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
-                System.out.println(dataSnapshot.child("hiscore").getValue());
+
+
+                if (dataSnapshot.child("hiscore").exists()) {
+                    uNames[count] = dataSnapshot.getKey();
+                    players[count].setText(dataSnapshot.getKey());
+                    tvScores[count].setText(dataSnapshot.child("hiscore").getValue().toString());
+
+                    System.out.println(Integer.valueOf(dataSnapshot.child("hiscore").getValue().toString()));
+                    hiscores[count] = Integer.valueOf(dataSnapshot.child("hiscore").getValue().toString());
+                    System.out.println(hiscores[count]);
+
+                    count--;
+
+                } else {
+
+                }
             }
 
             @Override
@@ -46,7 +126,25 @@ public class TopFiveActivity extends AppCompatActivity {
 
             }
 
+
             // ...
         });
+
+
+        System.out.println(uNames.length);
+        System.out.println(hiscores[4]);
+        System.out.println(count);
+
+        //System.out.println(userNames.size());
+        for (int i = 0; i < hiscores.length; i++) {
+            System.out.println(hiscores[i]);
+        }
+
+        for (int i = 0; i < uNames.length; i++) {
+            System.out.println(uNames[i]);
+        }
+//        for (String each: userNames) System.out.println(each);
+//        for (Integer each: scores) System.out.println(each);
+
     }
 }
--- hw_challenge/src/main/java/com/example/hw_challenge/hw_challenge.java
+++ hw_challenge/src/main/java/com/example/hw_challenge/hw_challenge.java
@@ -1,207 +1,228 @@
 package com.example.hw_challenge;
 
-import android.support.v4.app.ShareCompat;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.CompoundButton;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import java.util.ArrayList;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.util.Random;
 
+import static android.R.attr.right;
 
-/*
- In addition to the above, implement your own Second Grade Flash Card App.  The App will provide students with ten random math questions, either addition or subtraction.  For subtraction always ensure the top number is greater than or equal to the bottom.  Students will enter their response in a textbox.  Use a Single Activity.  Your program will utilize Java as the code-behind layer of your Activity and will generate 10 random problems, students will enter the answer into the textbox, then depress submit.  After 10 submissions the student’s score should popup in a Toast.
-We will improve upon the design later, don’t worry about telling students which problems they got right or wrong, or giving them immediate or delayed feedback.  Provide a descriptive Storyboard for your App.  Much of the design and user interaction is up to you, but feel free to use the template below as a guide.
- */
+public class hw_challenge extends AppCompatActivity {
 
-public class hw_challenge
-        extends AppCompatActivity {
-
-    private Button btnSubmit;
+    //following objects are the widgets showing in the app
+    private TextView tvNum1;
+    private TextView tvNum2;
+    private TextView tvSign;
     private Button btnGenerate;
-    private RadioButton rbtnAdd;
-    private RadioButton rbtnSub;
-    private TextView tvinPut1;
-    private TextView tvinPut2;
-    private TextView Sig;
-    private EditText edtResult;
-    private RadioGroup radioGrp;
-    private  simplelogin login;
+    private RadioGroup rg;
+    private RadioButton rb;
+    private EditText edtInput;
+    private Button btnSubmit;
+    private  TextView tvCurrent;
+    private Button btTop5;
     private String usr;
-    int checkid;
-    int countSubmit = 0;
-    int result;
-    int rightCount = 0;
+    DatabaseReference myRef;
 
-    ArrayList<Integer> rightAsn = new ArrayList<Integer>();
-    ArrayList<Integer> wrongAsn = new ArrayList<Integer>();
-    private static String LogFilterFlag = "Removing";
+    //following three arrays for saving the 10 math problems values
+    int[] a = new int[10];//10 random numbers
+    int[] b = new int[10];//10 random numbers
+    int[] res = new int[10];//the result of addition or subtraction for a and b
+    int right = 0;//counting the right numbers the user get
+    int count = 1;//recording the numbers until all the 10 problems are showing completely
+    // when changing the status of labels in the loop after user press the submit button
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_hw_challenge);
+        setContentView(R.layout.activity_main);
+        myRef = FirebaseDatabase.getInstance().getReference();
 
         Bundle extras = getIntent().getExtras();
         if(extras != null){
             usr = extras.getString("curr_username");
 
         }
-        Log.i("username", usr);
 
-        tvinPut1 = (TextView)findViewById(R.id.inPut1);
-        tvinPut2 = (TextView)findViewById(R.id.inPut2);
-
-        Sig = (TextView)findViewById(R.id.sig);
-        login = new simplelogin();
-        edtResult = (EditText)findViewById(R.id.result);
-        btnSubmit = (Button)findViewById(R.id.btnSubmit);
-        btnGenerate = (Button)findViewById(R.id.btnClickMe);
-        rbtnAdd = (RadioButton)findViewById(R.id.plusButton);
-        rbtnSub = (RadioButton)findViewById(R.id.subButton);
-        radioGrp = (RadioGroup)findViewById(R.id.radioGrp);
-        Bundle bundle = getIntent().getExtras();
-        String text= bundle.getString("username");
-        Toast toast_wel = Toast.makeText(getApplicationContext(),"Welcome " + text + " " +
-                                                                         "Congratulations." , Toast.LENGTH_SHORT);
-        toast_wel.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
-        toast_wel.show();
-
-        String S = "Please first select problem types first";
-        Toast.makeText(getBaseContext(),S,Toast.LENGTH_LONG).show();
-        radioGrp.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override public void onCheckedChanged(RadioGroup group, int checkedId) {
-                switch (checkedId) {
-                    case R.id.plusButton:
-                        Sig.setText("+");
-                        Sig.setScaleX(1);
-                        checkid = R.id.plusButton;
-                        break;
-                    case R.id.subButton:
-                        Sig.setText("-");
-                        Sig.setScaleX(2);
-                        checkid = R.id.subButton;
-                        break;
-                }
+        btTop5 = (Button) findViewById(R.id.btTop5);
+        btTop5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), TopFiveActivity.class);
+                i.putExtra("curr_username", usr);
+                startActivity(i);
             }
         });
 
+
+        Toast toast = Toast.makeText(getApplicationContext(), "Welcome " + usr, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 0);//put the toast in the center
+        toast.show();
+        btnGenerate = (Button) findViewById(R.id.btnGenerate);
+        rg = (RadioGroup) findViewById(R.id.radioGroup);
+        btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        tvNum1 = (TextView) findViewById(R.id.tvNum1);
+        tvNum2 = (TextView) findViewById(R.id.tvNum2);
+        tvSign = (TextView) findViewById(R.id.tvSign);
+        tvCurrent = (TextView) findViewById(R.id.tvCurrent);
+
+        //set a listener for the button generate 10 ...
         btnGenerate.setOnClickListener(new View.OnClickListener() {
-            @Override public void onClick(View v) {
-                edtResult.setText("");
-                switch (checkid) {
-                    case R.id.plusButton:
-                        Random r = new Random();
-                        int i1 = r.nextInt(50) + 1;
-                        int i2 = r.nextInt(50) + 1;
-                        tvinPut1.setText(String.valueOf(i1));
-                        tvinPut2.setText(String.valueOf(i2));
-                        result = i1 + i2;
+            @Override
+            public void onClick(View v) {
 
-                    case R.id.subButton:
-                        Random rs = new Random();
-                        int is1 = rs.nextInt(50) + 1;
-                        int is2 = rs.nextInt(50) + 1;
-                        if (is1 < is2) {
-                            int tempt = is1;
-                            is1 = is2;
-                            is2 = tempt;
+                myRef.child("Users").child(usr).orderByChild("test").limitToLast(1).addChildEventListener(new ChildEventListener() {
+                    @Override
+                    public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+
+                        if () System.out.println(dataSnapshot.getValue());
+                    }
+
+                    @Override
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+
+                    @Override
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+                    @Override
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+                    }
+
+
+                    // ...
+                });
+
+                int selectedId = rg.getCheckedRadioButtonId();//recording the status of radioGroup
+                Log.i("dd", String.valueOf(selectedId));
+
+                right = 0;
+                count = 1;
+                if (selectedId != -1) {//-1 means the user does not choose the radioButton
+                    rb = (RadioButton) findViewById(selectedId);
+                    String sign =(String) rb.getText();
+                    Random rand = new Random();
+
+                    if (sign.equals("Addition")) {//addition
+                        for (int i = 0; i < 10; i++) {//this loop saving the randomly generated numbers
+                            int aNumber = rand.nextInt(1000);//randomly generate numbers between 0-999
+                            int bNumber = rand.nextInt(1000);//same
+                            a[i] = aNumber;
+                            b[i] = bNumber;
                         }
 
-                        tvinPut1.setText(String.valueOf(is1));
-                        tvinPut2.setText(String.valueOf(is2));
-                        result = is1 - is2;
+                        for (int i = 0; i < 10; i++) {//this loop for recording the result of addtion of the two arrays
+                            res[i] = a[i] + b[i];
+                        }
 
-                    default:
-                        if (checkid != R.id.plusButton && checkid != R.id.subButton) {
-                            Toast.makeText(getBaseContext(),"Please select problem type first",Toast.LENGTH_LONG).show();
+                        //when user click the button generate 10 math problems, it will showing the first problem
+                        tvNum1.setText(String.valueOf(a[0]));
+                        tvNum2.setText(String.valueOf(b[0]));
+                        tvSign.setText("+");
+                        tvCurrent.setText("1/10");//showing the current number of problems the user is calculating
+
+                        //prompting the user start
+                        Toast toast = Toast.makeText(hw_challenge.this, "start", Toast.LENGTH_LONG);
+                        toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 170);
+                        toast.show();
+                    }
+                    else if (sign.equals("Subtraction")){//subtraction
+                        for (int i = 0; i < 10; i++) {//this loop saving the randomly generated numbers
+                            int bNumber = rand.nextInt(1000);//randomly generate numbers between 0-999
+                            int aNumber = rand.nextInt(1000 - bNumber) + bNumber;//randomly generate numbers between bNumber-999 so that the top number is always greater than or equal to the bottom one
+                            a[i] = aNumber;
+                            b[i] = bNumber;
+                        }
+
+                        for (int i = 0; i < 10; i++) {//this loop for recording the result of subtraction of the two arrays
+                            res[i] = a[i] - b[i];
                         }
 
 
+                        tvNum1.setText(String.valueOf(a[0]));
+                        tvNum2.setText(String.valueOf(b[0]));
+                        tvSign.setText("-");
+                        tvCurrent.setText("0/10");
+
+                        Toast toast = Toast.makeText(hw_challenge.this, "start", Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 170);
+                        toast.show();
+                    }
+                }
+                else {//if the user does not choose the sign, there will be a toast in order to remind the user of choosing the sign
+                    Toast toast = Toast.makeText(hw_challenge.this, "please choose the sign", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 170);
+                    toast.show();
                 }
             }
         });
 
-    }
+        //set a listener for the button submit
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int selectedId = rg.getCheckedRadioButtonId();
+                rb = (RadioButton) findViewById(selectedId);
+                edtInput = (EditText) findViewById(R.id.edtInput);
 
-    public void onClickSubmit(View v) {
-        if (countSubmit == 0) {
-            wrongAsn.clear();
-        }
+                String ans2 = edtInput.getText().toString();//get the text in the edtInput
+                if (ans2.equals("")){//if user does not input anything, there will be a toast prompting the user
+                    Toast toast = Toast.makeText(hw_challenge.this, "please input your answer", Toast.LENGTH_SHORT);
+                    toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 170);
+                    toast.show();
+                }else{
+                    int ans = Integer.parseInt(ans2);
+                    if (count <= 10 && ans == res[count-1]){//comparing the input with the array res, and if the ans is right, the right variable will self-add
+                        right++;
+                    }
 
 
-        String res = " ";
+                    edtInput.setText("");//each time initialize the text of edtInput for user convenience
 
-        if (!isEmpty(edtResult)){
-            if (edtResult.getText().toString().equals(String.valueOf(result))) {
-                countSubmit += 1;
-                res = "true";
-                rightCount += 1;
+                    if (count < 10) {//counting the numbers showing next, if haven't up to 10 problems, continue
+                        tvNum1.setText(String.valueOf(a[count]));
+                        tvNum2.setText(String.valueOf(b[count]));
+                        tvCurrent.setText(String.valueOf(count + 1) + "/10");//showing the current number user is calculating
+                        count++;
+                    }else{
+                        Toast toast = Toast.makeText(hw_challenge.this, "end", Toast.LENGTH_SHORT);
+                        toast.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 170);
+                        toast.show();
+
+                        //showing the right numbers user get out of the 10 problems
+                        Toast toast2 = Toast.makeText(hw_challenge.this, "Your score is " + right, Toast.LENGTH_LONG);
+                        toast2.setGravity(Gravity.CENTER|Gravity.CENTER_HORIZONTAL, 0, 170);
+                        toast2.show();
+
+                        myRef.child("Users").child(usr).child("hiscore").setValue(right);
+                    }
+                }
             }
-            else {
-                edtResult.setText(String.valueOf(result));
-                countSubmit += 1;
-                res = "false";
-                wrongAsn.add(countSubmit);
-            }
-            Toast.makeText(getBaseContext(),"You submitted problem " + countSubmit + ". The Answer " +
-                                                    "is " + res,Toast.LENGTH_SHORT).show();
-
-        }
-        else {
-            Toast toast = Toast.makeText(getApplicationContext(),"Please enter your answer",Toast.LENGTH_SHORT);
-            toast.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
-            toast.show();
-        }
-
-
-        // this is to reset the process
-        if (countSubmit == 10) {
-            StringBuilder builder = new StringBuilder();
-            for (Integer value : wrongAsn) {
-                builder.append(value);
-            }
-            String wrongtext = builder.toString();
-
-            Toast toast2 = Toast.makeText(getApplicationContext(),"Your Score is " + rightCount + ". The wrong problems are: " + wrongAsn + ",",Toast
-                                                                                                      .LENGTH_SHORT);
-            toast2.setGravity(Gravity.CENTER_HORIZONTAL, 0, 0);
-
-            LinearLayout toastLayout = (LinearLayout) toast2.getView();
-            TextView toastTV = (TextView) toastLayout.getChildAt(0);
-            toastTV.setTextSize(30);
-            toast2.show();
-            countSubmit = 0;
-
-
-        }
-
-
-
-
+        });
     }
-
-    public void onDestroy() {
-        super.onDestroy();
-        Log.e(LogFilterFlag, "onDestroy Called.");
-    }
-
-    private boolean isEmpty(EditText etText) {
-        if (etText.getText().toString().trim().length() > 0)
-        {
-            return false;
-        }
-        return true;
-    }
-
-
 }
+
