48f7190ba09de18a73d51434121cefcbb4599d33
qm930708 <qm930708@bu.edu>
2017-04-03 20:15:56 -0400

    get top five

--- hw_challenge/src/main/java/com/example/hw_challenge/TopFiveActivity.java
+++ hw_challenge/src/main/java/com/example/hw_challenge/TopFiveActivity.java
@@ -3,11 +3,50 @@ package com.example.hw_challenge;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class TopFiveActivity extends AppCompatActivity {
+
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.top_five);
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        myRef.child("Users").orderByChild("hiscore").limitToLast(5).addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String prevChildKey) {
+                System.out.println(dataSnapshot.child("hiscore").getValue());
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            // ...
+        });
     }
 }
